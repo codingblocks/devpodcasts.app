43ef77f2bb11d3ea2db0f6f3c5eb201e49575eef
THEjoezack <me@joezack.com>
2018-12-29 14:20:02 -0500

    bah, fixing link

--- src/components/hero.js
+++ src/components/hero.js
@@ -8,7 +8,7 @@ const Hero = () => (
       <h3>Listen to podcast episodes on tags you care about</h3>
       <ul className='discovery-list'>
         <li>
-          <Link to='https://qit.cloud'>search</Link>
+          <a href='https://qit.cloud'>search</a>
         </li>
         <li className='discovery-list-spacer'>|</li>
         <li>
