ad475bef14a72a53768c3e90d7ebf936fb7ffe98
THEjoezack <me@joezack.com>
2019-01-05 21:24:35 -0500

    Working around the incorrect ordering of stylesheets, I think

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.css'
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
@@ -6,7 +5,6 @@ import { StaticQuery, graphql } from 'gatsby'
 import Navbar from './navbar'
 import Sponsors from '../components/sponsors'
 import Footer from './footer'
-import './static/css/flexslider.css'
 import './static/css/style.css'
 
 const Layout = ({ children }) => (
